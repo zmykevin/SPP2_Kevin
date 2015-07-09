79a5275b0108cb138149f872052212a4e56e5106
kevin Zhou <zmykevin@umich.edu>
2015-07-09 11:16:21 -0400

    laser pointer code

---
+++ My Laser Pointer Tracker/laser_pointer_tracker.py
@@ -0,0 +1,25 @@
+__author__ = 'kevin'
+import numpy as np
+import cv2
+
+radius = 5
+cap = cv2.VideoCapture(1)
+#Step 1 Create a bar to control whether to start the program
+
+#If the switch is turned on start to track the brightest point in the frame
+while(True):
+    ret,frame = cap.read()
+    gray_original = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray_original,(radius,radius),0)
+    (minVal, maxVal, minLoc,maxLoc) = cv2.minMaxLoc(gray)
+    print("max vlue is: %d"% (maxVal))
+    print(maxLoc)
+    cv2.circle(frame,maxLoc,5,(255,0,0),2)
+    cv2.imshow("bright_spot",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
