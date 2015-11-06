51e2a866696558fd386e03e5b987db2514c46a4d
Mingyang Zhou <zmykevin@umich.edu>
2015-11-05 23:16:12 -0500

    basic structure is setup

--- My Laser Pointer Tracker/laser_tracker.cpp
+++ My Laser Pointer Tracker/laser_tracker.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/highgui/highgui.hpp>
 #include <opencv/cv.h>
 #include <iostream>
 #include <stdio.h>
--- background subtraction/c++/background_subtraction_main.cpp
+++ background subtraction/c++/background_subtraction_main.cpp
@@ -1,60 +1,60 @@
-#include "opencv2/highgui.hpp"
-#include "opencv2/videoio.hpp"
-#include "opencv2/opencv.hpp"
-#include "opencv2/core.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace cv;
 using namespace std;
-
-class Gaussian_i
+struct mixture_gaussian
 {
-public:
-    Mat mean;
-    Mat stddev;
-    Mat weights;
-    Gaussian_i(int col, int row){
-        mean = Mat(col, row, CV_8U, Scalar(0));
-        stddev = Mat(col, row, CV_64F, Scalar(0));
-        weights = Mat(col, row, CV_64F, Scalar(0));
-    }
+vector<double> Mean;
+vector<double> Std;
+vector<double> Weight;
+vector<double> W_S;
+}
+int main()
+{
+cout << "Background Subtraction Start" << endl;
 
-};
+// Define the number of gaussina models in the mixed model
+int K = 5;
+// Define the learning reate
+double alpha = 0.05;
+// Define initial weight parameter
+double i_weight = 0.2;
 
-// bool sortMat (Mat i,Mat j) { return (i<j); }
+// Start to capture the video
+VideoCapture cap("highwayI_raw.AVI");
 
-int background_process_heuristic(vector<Gaussian_i>*, double);
+// Get the size dimension of the frame
+int frame_width = (int)cap.get(CAP_PROP_FRAME_WIDTH);
+int frame_height = (int)cap.get(CAP_PROP_FRAME_HEIGHT);
 
-int main(){
+vector<mixture_gaussian> miture_gaussian_model();
+Mat initial_frame;
+cap >> initial_frame;
+Mat initial_gray_frame;
+cvtColor(initial_frame,intial_gray_frame,COLOR_BGR2GRAY);
 
-    cout << "Background Subtraction Start" << endl;
-    /*
-    // Define the number of gaussina models in the mixed model
-    int K = 5;
-    // Define the learning reate
-    double alpha = 0.05;
-    // Define initial weight parameter
-    double i_weight = 0.2;
 
-    // Start to capture the video
-    VideoCapture cap("highwayI_raw.AVI");
+while(1)
+{
+    Mat frame;
+    cap >> frame;
+    Mat gray_frame;
+    cvtColor(frame,gray_frame,COLOR_BGR2GRAY);
+    imshow("video",gray_frame);
 
-    // Get the size dimension of the frame
-    int frame_width = (int)cap.get(CAP_PROP_FRAME_WIDTH);
-    int frame_height = (int)cap.get(CAP_PROP_FRAME_HEIGHT);
-    */
-    // cout<<frame_width<<endl;
-    // cout<<frame_height<<endl;
+    if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
+   {
+    cout << "esc key is pressed by user" << endl;
+    break;
+   }
+}
 
-    /*
-    while(1)
-    {
-        cap >> frame;
-        Mat gray_frame;
-        cvtColor(frame,gray_frame,COLOR_BGR2GRAY);
-        imshow("video",gray_frame);
-    }
-    */
-    }
+
+return 0;
+}
