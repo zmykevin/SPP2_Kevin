d9033d49db2158485123c06e57b99b03d822d2ab
Mingyang Zhou <zmykevin@umich.edu>
2015-11-17 00:49:53 -0500

    Include update for group operation the initial result looks good

--- background subtraction/c++/background_subtraction_main_v2.cpp
+++ background subtraction/c++/background_subtraction_main_v2.cpp
@@ -13,7 +13,9 @@ using namespace cv;
 using namespace std;
 
 //backgournd Gaussian Process helps to rerank the gaussian_weight
-Mat backgroundGaussianProcess(Mat* Gaussian_Mean, Mat* Gaussian_std, Mat* Gaussian_weight, Mat threshold);
+Mat compare_to_scalar(Mat matrix_1, int scalar);
+void Sort_Gaussian(Mat* Gaussian_Mean, Mat* Gaussian_std, Mat* Gaussian_weight);
+
 int main()
 {
 	cout << "background subtraction" << endl;
@@ -78,9 +80,9 @@ while(1)
     //Start the main process here
 //--------------------------Main Process-------------------------------------//
     //Create a Threshold
-    Mat T = Mat::ones(K,frame_width*frame_height,CV_64F)*0.1;
-    backgroundGaussianProcess(&gaussian_mean,&gaussian_std,&gaussian_weight,T);
-    if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
+    //Mat T = Mat::ones(K,frame_width*frame_height,CV_64F)*0.1;
+    Sort_Gaussian(&gaussian_mean,&gaussian_std,&gaussian_weight);
+    if(waitKey(1) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
    {
     cout << "ESC Key is pressed by user" << endl;
     break;
@@ -90,10 +92,53 @@ while(1)
 	return 0;
 }
 
-Mat backgroundGaussianProcess(Mat* Gaussian_Mean, Mat* Gaussian_std, Mat* Gaussian_weight, Mat threshold)
+Mat compare_to_scalar(Mat matrix_1, int scalar)
+{
+	//First of all, convert it to a specific type
+	matrix_1.convertTo(matrix_1,CV_64F);
+	//Construct a scalar matrix
+	Mat scalar_matrix = Mat::ones(matrix_1.size[0],matrix_1.size[1],CV_64F)*double(scalar);
+	//Now Compare the two matrix
+	Mat compare_result;
+	compare(matrix_1,scalar_matrix,compare_result,CMP_EQ);
+	compare_result = compare_result/double(255);
+	compare_result.convertTo(compare_result,CV_64F);
+	return compare_result;
+}
+
+void Sort_Gaussian(Mat* Gaussian_mean, Mat* Gaussian_std, Mat* Gaussian_weight)
 {
 	Mat result;
-	int num_gaussian = Gaussian_Mean->size[0];
-	cout << num_gaussian << endl;
-	return result;
+	int num_gaussian = Gaussian_mean->size[0];
+	Mat W_by_SD = Mat::zeros(num_gaussian,Gaussian_mean->size[1],CV_64F);
+	for (int i = 0; i < num_gaussian; i ++)
+	{
+		Mat division_result;
+		divide((*Gaussian_weight).row(i),(*Gaussian_std).row(i),division_result);
+		division_result.copyTo(W_by_SD.row(i));
+	}
+	//Sort W_by_SD and return index
+	Mat sort_index;
+	sortIdx(W_by_SD,sort_index,SORT_EVERY_COLUMN+SORT_DESCENDING);
+	//The next step is to reorder all the parameters with respect to this rank
+	Mat original_gaussian_mean = (*Gaussian_mean).clone();
+	Mat original_gaussian_std = (*Gaussian_std).clone();
+	Mat original_gaussian_weight = (*Gaussian_weight).clone();
+	for (int i = 0; i < num_gaussian; i ++)
+	{
+		Mat new_gaussian_mean_i = Mat::zeros(1,Gaussian_mean->size[1],CV_64F);
+		Mat new_gaussian_std_i = Mat::zeros(1,Gaussian_std->size[1],CV_64F);
+		Mat new_gaussian_weight_i = Mat::zeros(1,Gaussian_weight->size[1],CV_64F);
+		for (int j = 0; j < num_gaussian; j ++)
+		{
+			Mat comparison_result = compare_to_scalar(sort_index.row(i),j);
+			new_gaussian_mean_i += (original_gaussian_mean.row(j)).mul(comparison_result);
+			new_gaussian_weight_i += (original_gaussian_weight.row(j)).mul(comparison_result);
+			new_gaussian_std_i += (original_gaussian_std.row(j)).mul(comparison_result);
+		}
+		new_gaussian_mean_i.copyTo(Gaussian_mean->row(i));
+		new_gaussian_weight_i.copyTo(Gaussian_weight->row(i));
+		new_gaussian_std_i.copyTo(Gaussian_std->row(i));
+	}
 }
+
